5f76cdef34cc54f54792543b6eec0232ae4beb20
Rajbhar Divesh <84081602+DiveshRDH@users.noreply.github.com>
2025-04-18 00:22:02 +0530

    Add files via upload

---
+++ User_Setup.h
@@ -0,0 +1,16 @@
+#define ILI9341_DRIVER
+
+#define TFT_CS   15
+#define TFT_DC    2
+#define TFT_RST   4
+
+#define TFT_MOSI 13
+#define TFT_SCLK 14
+#define TFT_MISO -1
+
+#define LOAD_GLCD
+#define LOAD_FONT2
+#define LOAD_FONT4
+
+#define SPI_FREQUENCY  27000000
+
